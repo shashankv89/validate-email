55e49b70ca5eebbf59648d30d49fa4241e06facb
shashank <v.shashank77@gmail.com>
2023-03-23 20:18:59 +0530

    bug 120 added and fixed

--- myapp.java
+++ myapp.java
@@ -1,6 +1,28 @@
+<<<<<<< Updated upstream
 my application
 
 //bug 122-changes
 
 
 //logic done
+=======
+<html>
+
+
+<head>
+
+<script>
+
+//bug -100 fix cahnges
+
+function confirm(){
+	
+	int a=10;
+	int b=20;
+	
+	//logic pending
+}
+
+
+my application
+>>>>>>> Stashed changes
